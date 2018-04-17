13cc4ca4c0b5df81bfda6663fe9f3f625b66b467
Matt Burrows <mburrows@batstrading.com>
2018-04-17 09:48:37 +0100

    Latest changes

--- dotfiles/config.fish
+++ dotfiles/config.fish
@@ -1,4 +1,4 @@
-# vim: set ft=sh foldmethod=marker:
+# vim: set ft=fish foldmethod=marker:
 
 # Abbreviations {{{
 abbr -a gs="git status"
@@ -8,9 +8,27 @@ abbr -a gc="git commit"
 abbr -a gr="git rebase"
 abbr -a gz="git stash"
 abbr -a gp="git push"
-abbr -a gu="git push"
+abbr -a gf="git pull"
 abbr -a gv="git svnup"
 abbr -a gl="git log"
 abbr -a g1="git log --oneline"
-abbr -a ga="git add -u"
+abbr -a gu="git add -u"
 # }}}
+
+# Aliases {{{
+alias g="git --no-pager grep -n --break"
+alias rl="source ~/.config/fish/config.fish"
+# }}}
+
+# Functions {{{
+# }}}
+
+# Miscellaneous {{{
+set -x GTAGSFORCECPP 1
+
+# Load BATS configuration (it it exists)
+if test -f ~/.config/fish/bats.fish
+    source ~/.config/fish/bats.fish
+end
+# }}}
+
